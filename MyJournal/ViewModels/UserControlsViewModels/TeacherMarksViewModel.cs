bb07fa20146a7fb13de66b243efc4c6bf1184e0f
fokypoky <petuhova805@gmail.com>
2023-04-20 22:47:01 +0300

    Сделал обработчик нажатия на "класс". Теперь необходимо получить "предмет".

--- MyJournal/ViewModels/UserControlsViewModels/TeacherMarksViewModel.cs
+++ MyJournal/ViewModels/UserControlsViewModels/TeacherMarksViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using MyJournal.Infrastructure.Commands;
 using MyJournal.Models;
 using MyJournal.ViewModels.Base;
 using MyJournalLibrary.Entities;
@@ -10,19 +13,43 @@ namespace MyJournal.ViewModels.UserControlsViewModels;
 public class TeacherMarksViewModel : ViewModel
 {
     private List<Subject> _subjects;
-    private List<Class> _classes;
+    private Subject _expandedSubject;
     
+    public ICommand OnSubjectClassTreeViewSelectedItemChanged
+    {
+        get => new RelayCommand((object parameter) =>
+        {
+            if (parameter is Subject)
+            {
+                MessageBox.Show("SUBJECT");
+            }
+            if (parameter is Class)
+            {
+                Class selectedClass = (Class)parameter;
+                // остальная логика...
+                MessageBox.Show("CLASS");
+            }
+        });
+    }
+
+    public ICommand OnSubjectClassTreeViewItemExpanded
+    {
+        get => new RelayCommand((object parameter) =>
+        {
+            MessageBox.Show("EXPANDED");
+            /*if (parameter is Subject)
+            {
+                _expandedSubject = (Subject)parameter;
+            }*/
+        });
+    }
+
     public List<Subject> Subjects
     {
         get => _subjects;
         set => SetField(ref _subjects, value);
     }
-
-    public List<Class> Classes
-    {
-        get => _classes;
-        set => SetField(ref _classes, value);
-    }
+    
     public TeacherMarksViewModel()
     {
         using (var context = new ApplicationContext())
