2cafce42ec9e90b9647747b10628dd1c05532312
Anton Petukhov <petuhova805@gmail.com>
2023-05-13 15:12:23 +0300

    1. Расширил репозиторий оценок; 2. В ViewModel окна проставления оценок получил все оценки выбранного класса и предмета.

--- MyJournal/ViewModels/TeacherMarksWindowViewModel.cs
+++ MyJournal/ViewModels/TeacherMarksWindowViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using MyJournal.Models;
 using MyJournal.ViewModels.Base;
 using MyJournalLibrary.Entities;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournal.ViewModels;
 
@@ -10,6 +14,7 @@ public class TeacherMarksWindowViewModel : ViewModel
     private string _windowTitle;
     private Class _selectedClass;
     private Subject _selectedSubject;
+    private List<Mark> _marks;
     
     public string WindowTitle
     {
@@ -17,11 +22,23 @@ public class TeacherMarksWindowViewModel : ViewModel
         set => SetField(ref _windowTitle, value);
     }
 
-    public TeacherMarksWindowViewModel()
+    public List<Mark> Marks
     {
-        WindowMessanger.MessageSender += OnMessageReceived;
+        get => _marks;
+        set => SetField(ref _marks, value);
     }
+    
+    private void LoadInfo()
+    {
+        using (var context = new ApplicationContext())
+        {
+            Marks = new MarksRepository(context)
+                .GetMarksByClassAndSubject(_selectedClass, _selectedSubject)
+                .ToList();
+        }
 
+        MessageBox.Show(Marks?.Count.ToString());
+    }
     private void OnMessageReceived(object sender, EventArgs e)
     {
         if (e is ClassSubjectMessage)
@@ -31,6 +48,13 @@ public class TeacherMarksWindowViewModel : ViewModel
 
             WindowTitle = $"{_selectedSubject.SubjectTitle} - {_selectedClass.ClassNumber}";
             WindowMessanger.MessageSender -= OnMessageReceived;
+            
+            LoadInfo();
         }
+    }
+    
+    public TeacherMarksWindowViewModel()
+    {
+        WindowMessanger.MessageSender += OnMessageReceived;
     }
 }
--- MyJournalLibrary/Repositories/EntityRepositories/MarksRepository.cs
+++ MyJournalLibrary/Repositories/EntityRepositories/MarksRepository.cs
@@ -8,4 +8,15 @@ public class MarksRepository : EntityRepository<Mark>
     public MarksRepository(DbContext context) : base(context)
     {
     }
+
+    public ICollection<Mark> GetMarksByClassAndSubject(Class @class, Subject subject)
+    {
+        return _context.Set<Mark>()
+            .Include(m => m.Subject)
+            .Include(m => m.Student)
+                .ThenInclude(s => s.Class)
+            .Where(m => m.Student.Class == @class && m.Subject == subject)
+            .ToList();
+    }
+    
 }
