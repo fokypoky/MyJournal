aa5c8e2118db564f97cb386059b67670abd9f822
Anton Petukhov <petuhova805@gmail.com>
2023-07-01 14:45:16 +0300

    1. Выборка годов и месяцев оценок по предмету, привязка ко View; 2. Рефакторинг вьюмодели

--- MyJournal/ViewModels/TeacherMarksWindowViewModel.cs
+++ MyJournal/ViewModels/TeacherMarksWindowViewModel.cs
@@ -21,55 +21,59 @@ public class TeacherMarksWindowViewModel : ViewModel
     private Subject _selectedSubject;
     private ObservableCollection<Student> _students;
 
+    private int _selectedYear;
+    private int _selectedMonth;
+    private ObservableCollection<int> _markYears;
+    private ObservableCollection<int> _markMonths;
+
     private DataTable _marksTable;
 
+    public int SelectedYear
+    {
+        get => _selectedYear;
+        set => SetField(ref _selectedYear, value);
+    }
+    public int SelectedMonth
+    {
+        get => _selectedMonth;
+        set => SetField(ref _selectedMonth, value);
+    }
     public DataView MarksDataView
     {
         get => _marksTable.DefaultView;
-        set => _marksTable = value.ToTable();
     }
-
-    public ObservableCollection<Student> Students
-    {
-        get => _students;
-        set => SetField(ref _students, value);
-    }
-
     public string WindowTitle
     {
         get => _windowTitle;
         set => SetField(ref _windowTitle, value);
     }
 
-    #region data initialization
+    #region Collections
+    public ObservableCollection<Student> Students
+    {
+        get => _students;
+        set => SetField(ref _students, value);
+    }
+    public ObservableCollection<int> MarkYears
+    {
+        get => _markYears;
+        set => SetField(ref _markYears, value);
+    }
+    public ObservableCollection<int> MarkMonths
+    {
+        get => _markMonths;
+        set => SetField(ref _markMonths, value);
+    }
+    #endregion
 
+    #region Commands
     public ICommand LoadCommand
     {
         get => new RelayCommand((object p) =>
         {
-            //_marksTable.Clear();
-            //_marksTable = new DataTable();
-
-            //DataColumn studentsColumn = new DataColumn();
-            //studentsColumn.ColumnName = "Ученики";
-
-            //_marksTable.Columns.Add(studentsColumn);
-
-            //foreach (var student in Students)
-            //{
-            //    DataRow row = _marksTable.NewRow();
-
-            //    string studentName = $"{student.Contacts.Surname} {student.Contacts.Name} {student.Contacts.Midname}";
-
-            //    row[0] = studentName;
-
-            //    _marksTable.Rows.Add(row);
-            //}
-
-            //OnPropertyChanged("MarksDataView");
+            
         });
     }
-
     public ICommand AddMarkColumnCommand
     {
         get => new RelayCommand((object parameter) =>
@@ -83,45 +87,88 @@ public class TeacherMarksWindowViewModel : ViewModel
                     return;
                 }
             }
-
-            var tempTable = new DataTable();
-
-            for (int i = 0; i < _marksTable.Columns.Count; i++)
-            {
-                var column = new DataColumn(_marksTable.Columns[i].ColumnName);
-                tempTable.Columns.Add(column);
-            }
-
-            for (int i = 0; i < _marksTable.Rows.Count; i++)
-            {
-                var rowValues = new List<string>();
-
-                foreach (var rowValue in _marksTable.Rows[i].ItemArray)
-                {
-                    rowValues.Add(rowValue.ToString());
-                }
-
-                var newRow = tempTable.NewRow();
-
-                for (int j = 0; j < rowValues.Count; j++)
-                {
-                    newRow[j] = rowValues[j];
-                }
-
-                tempTable.Rows.Add(newRow);
-            }
-
-            var newMarkColumn = new DataColumn(todayDate);
-            tempTable.Columns.Add(newMarkColumn);
-
-            _marksTable.Clear();
-            _marksTable = null;
-            _marksTable = tempTable;
-
+            AddMarkColumn(todayDate);
             OnPropertyChanged("MarksDataView");
         });
     }
+    #endregion
+    
+    #region data initialization
+    private void AddMarkColumn(string columnTitle)
+    {
+        var tempTable = new DataTable();
+        for (int i = 0; i < _marksTable.Columns.Count; i++)
+        {
+            var column = new DataColumn(_marksTable.Columns[i].ColumnName);
+            tempTable.Columns.Add(column);
+        }
 
+        for (int i = 0; i < _marksTable.Rows.Count; i++)
+        {
+            var rowValues = new List<string>();
+
+            foreach (var rowValue in _marksTable.Rows[i].ItemArray)
+            {
+                rowValues.Add(rowValue.ToString());
+            }
+
+            var newRow = tempTable.NewRow();
+
+            for (int j = 0; j < rowValues.Count; j++)
+            {
+                newRow[j] = rowValues[j];
+            }
+
+            tempTable.Rows.Add(newRow);
+        }
+
+        //var newMarkColumn = new DataColumn(columnTitle);
+        tempTable.Columns.Add(new DataColumn(columnTitle));
+
+        _marksTable.Clear();
+        _marksTable = null;
+        _marksTable = tempTable;
+    }
+    private void AddMarkColumnsRange(ICollection<string> columnTitles)
+    {
+        var tempTable = new DataTable();
+        for (int i = 0; i < _marksTable.Columns.Count; i++)
+        {
+            var column = new DataColumn(_marksTable.Columns[i].ColumnName);
+            tempTable.Columns.Add(column);
+        }
+
+        for (int i = 0; i < _marksTable.Rows.Count; i++)
+        {
+            var rowValues = new List<string>();
+
+            foreach (var rowValue in _marksTable.Rows[i].ItemArray)
+            {
+                rowValues.Add(rowValue.ToString());
+            }
+
+            var newRow = tempTable.NewRow();
+
+            for (int j = 0; j < rowValues.Count; j++)
+            {
+                newRow[j] = rowValues[j];
+            }
+
+            tempTable.Rows.Add(newRow);
+        }
+
+        foreach (var columnTitle in columnTitles)
+        {
+            if (!_marksTable.Columns.Contains(columnTitle))
+            {
+                tempTable.Columns.Add(new DataColumn(columnTitle));
+            }
+        }
+        
+        _marksTable.Clear();
+        _marksTable = null;
+        _marksTable = tempTable;
+    }
     private void OnMessageReceived(object sender, EventArgs e)
     {
         if (e is ClassSubjectMessage)
@@ -145,6 +192,19 @@ public class TeacherMarksWindowViewModel : ViewModel
                 .OrderBy(s => s.Contacts.Surname));
         }
 
+        var dates = (
+            from student in Students
+            from mark in student.Marks
+            where mark.SubjectId == _selectedSubject.Id
+            select mark.MarkDate).Distinct();
+
+        MarkYears = new ObservableCollection<int>(
+            (from date in dates select date.Year).Distinct()
+        );
+        MarkMonths = new ObservableCollection<int>(
+            (from date in dates select date.Month).Distinct()
+        );
+        
         InitializeMarksDataTable();
     }
 
