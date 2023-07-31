f833d600aca498fcc00fbbb5a096b62b07fe8880
Anton Petukhov <petuhova805@gmail.com>
2023-07-31 06:13:15 +0300

    Рефакторинг

--- SQL/dbscript.sql
+++ SQL/dbscript.sql
@@ -107,10 +107,6 @@ VALUES ('32-А'),('45'),
        ('37'),('10-Б'),
        ('30'),('29');
     -- классы
-INSERT INTO Classes(class_number, leader_id, auditory_id)
-VALUES ('11-А', 1, 1),
-       ('10-Б', 2, 2);
-    -- контакты учителей
 INSERT INTO Contacts(surname, name, midname, phone_number, email, password, sex)
 VALUES ('admin', 'admin', NULL, 'admin', 'admin', 'admin', 'М'),
        ('Шнюк', 'Фулиона', 'Сергеевна', '+79108883288', 'shnyuk@gmail.com', 'shnyuk', 'Ж'),
@@ -132,45 +128,45 @@ VALUES ('admin', 'admin', NULL, 'admin', 'admin', 'admin', 'М'),
 
 -- добавляю сотрудников
 INSERT INTO Employees(contacts_id) (SELECT ID FROM contacts WHERE id <= 5);
+
+INSERT INTO Classes(class_number, leader_id, auditory_id)
+VALUES ('11-А', 1, 1),
+       ('10-Б', 2, 2);
+    -- контакты учителей
 -- добавляю родителей
 INSERT INTO Parents(contacts_id) (SELECT ID FROM Contacts WHERE ID > 10 AND ID < 16);
 -- добавляю учеников
 INSERT INTO Students(class_id, contacts_id)
-VALUES (1, 6), (1, 7),
-       (1,8), (2,9), (2,10);
+VALUES (5, 6), (5, 7),
+       (5,8), (6,9), (6,10);
 -- добавляю задания
 INSERT INTO Tasks(SUBJECT_ID, CLASS_ID, TASK_TEXT, TEACHER_ID)
-VALUES (1, 1, 'Страница 190 задачи 11-15', 1),
-       (2, 1, 'Электричество и магнитизм', 1),
-       (3, 1, 'Функции и методы C#, перегрузка, рекурсия', 1);
+VALUES (1, 5, 'Страница 190 задачи 11-15', 1),
+       (2, 5, 'Электричество и магнитизм', 1),
+       (3, 5, 'Функции и методы C#, перегрузка, рекурсия', 1);
 -- добавляю оценки
 INSERT INTO Marks(student_id, mark, mark_date, teacher_id, task_id)
-VALUES (1, 5, NOW(), 1, 1),
-       (2, 5, NOW(), 1, 1),
-       (3, 2, now(), 1, 1),
+VALUES (6, 5, NOW(), 1, 4),
+       (7, 5, NOW(), 1, 4),
+       (8, 2, now(), 1, 4),
 
-       (1, 2, NOW(), 1, 2),
-       (2, 2, NOW(), 1, 2),
-       (3, 2, now(), 1, 2),
+       (6, 2, NOW(), 1, 5),
+       (7, 2, NOW(), 1, 5),
+       (8, 2, now(), 1, 5),
 
-       (1, 3, NOW(), 1, 3),
-       (2, 5, NOW(), 1, 3),
-       (3, 4, now(), 1, 3);
+       (6, 3, NOW(), 1, 6),
+       (7, 5, NOW(), 1, 6),
+       (8, 4, now(), 1, 6);
 --добавляю расписание
-SELECT * FROM Employees e JOIN Contacts C on C.id = e.contacts_id;
-SELECT * FROM Classes;
-SELECT * FROM Subjects;
-SELECT * FROM Auditories;
-
 INSERT INTO Timetable(day_of_week, lesson_time, class_id, subject_id, auditory_id, teacher_id)
-VALUES (1, '10:00:00', 1, 1, 5, 1),
-       (2, '10:00:00',1, 3, 6, 1);
+VALUES (1, '10:00:00', 5, 1, 5, 1),
+       (2, '10:00:00',5, 3, 6, 1);
 
 -- связываю учеников и родителей
 INSERT INTO Parent_student(parent_id, student_id)
-VALUES (1,1), (2,2),
-       (3,3), (4,4),
-       (5,5);
+VALUES (1,6), (2,7),
+       (3,8), (4,9),
+       (5,10);
 -- связываю учителей и предметы
 INSERT INTO Employee_Subject(employee_id, subject_id)
 VALUES (1, 1), (1, 2), (1,3),
@@ -191,23 +187,20 @@ CREATE TABLE Class_Subject(
   FOREIGN KEY (Class_id) REFERENCES Classes(ID),
   FOREIGN KEY (Subject_id) REFERENCES Subjects(ID)
 );
+
 INSERT INTO Class_Subject(Class_id, Subject_id)
-VALUES (1, 3),
-       (1, 2),
-       (1,1);
-SELECT * FROM Class_Subject
-SELECT * FROM Tasks t JOIN Classes C on C.id = t.class_id
-        JOIN Subjects ON t.subject_id = Subjects.id
+VALUES (5, 3),
+       (5, 2),
+       (5,1);
 
 INSERT INTO UserRoles(rolename)
 VALUES ('employee'), ('student'), ('parent'), ('admin');
 
 ALTER TABLE Contacts ADD COLUMN userrole_id INT;
 
-UPDATE Contacts SET userrole_id = 1 WHERE ID < 6;
-UPDATE Contacts SET userrole_id = 2 WHERE ID > 5 AND ID < 11;
-UPDATE Contacts SET userrole_id = 3 WHERE ID > 10 AND ID <= 15;
-
+UPDATE Contacts SET userrole_id = 1 WHERE ID IN (SELECT contacts_id FROM Employees);
+UPDATE Contacts SET userrole_id = 2 WHERE ID IN (SELECT contacts_id FROM Students);
+UPDATE Contacts SET userrole_id = 3 WHERE ID IN (SELECT contacts_id FROM Parents);
 
 -- индексы
 CREATE UNIQUE INDEX UX_Timetable_lesson_day_date ON Timetable(day_of_week, lesson_time);
@@ -217,9 +210,9 @@ CREATE UNIQUE INDEX UX_Timetable_lesson_day_date ON Timetable(day_of_week, lesso
 
 ALTER TABLE Marks ADD COLUMN subject_id INT;
 
-UPDATE Marks SET subject_id = 1 WHERE task_id = 1;
-UPDATE Marks SET subject_id = 2 WHERE task_id = 2;
-UPDATE Marks SET subject_id = 3 WHERE task_id = 3;
+UPDATE Marks SET subject_id = 1 WHERE task_id = 4;
+UPDATE Marks SET subject_id = 2 WHERE task_id = 5;
+UPDATE Marks SET subject_id = 3 WHERE task_id = 6;
 
 ALTER TABLE Marks
     ADD CONSTRAINT fk_marks_subjects FOREIGN KEY (subject_id) REFERENCES Subjects (id);
@@ -227,28 +220,17 @@ ALTER TABLE Marks
 ALTER TABLE Marks
     ALTER COLUMN subject_id SET NOT NULL;
 
-SELECT * FROM Marks m JOIN Tasks T on m.task_id = T.id WHERE m.mark = 2;
-SELECT * FROM Subjects;
-
-SELECT * FROM Tasks WHERE subject_id = 2 AND class_id = 1;
-SELECT * FROM Marks WHERE task_id = 2;
-SELECT * FROM Marks m JOIN Subjects s ON m.subject_id = s.id;
-
-SELECT * FROM Marks;
-SELECT * FROM Tasks;
-SELECT * FROM Classes;
-SELECT * FROM Subjects;
+ALTER TABLE Tasks ADD COLUMN task_start_date timestamp;
+ALTER TABLE Tasks ADD COLUMN task_deadline_date timestamp;
 
 INSERT INTO Tasks(subject_id, class_id, task_text, teacher_id, task_start_date, task_deadline_date)
-    VALUES(2, 1, 'Молекулярная физика. Как спариваются микробы?', 1, '2023-07-18', '2023-07-25')
+    VALUES(2, 5, 'Молекулярная физика. Как спариваются микробы?', 1, '2023-07-18', '2023-07-25')
 
 INSERT INTO Marks(student_id, mark, mark_date, teacher_id, task_id, subject_id)
-    VALUES(1, 4, '2023-07-18', 1, 5, 2)
+    VALUES(6, 4, '2023-07-18', 1, 5, 2);
 
-SELECT * FROM Marks
 -- TODO: снова добавить ограничение NOT NULL
 ALTER TABLE Marks ALTER COLUMN task_id DROP NOT NULL;
 
-SELECT * FROM marks
 -- YEAR = 2023; MONTH = 3
 -- UPDATE Tasks SET task_start_date = '2023-03-10', task_deadline_date = '2023-03-18' WHERE id = 2;
