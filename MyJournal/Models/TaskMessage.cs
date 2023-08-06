9a5647a0278949ddf19602b702a1f7a766b3edb4
Anton Petukhov <petuhova805@gmail.com>
2023-08-06 18:46:36 +0300

    Межоконное сообщение с заданием

---
+++ MyJournal/Models/TaskMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task = MyJournalLibrary.Entities.Task;
+
+namespace MyJournal.Models;
+
+public class TaskMessage : EventArgs
+{
+    public Task Task = null!;
+    public TaskMessageType Type;
+}
