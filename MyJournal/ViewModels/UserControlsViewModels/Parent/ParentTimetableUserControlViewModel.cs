249b8459f703fd3270a45ae2fdf855a12d0cb4bb
Anton Petukhov <petuhova805@gmail.com>
2023-08-10 21:55:39 +0300

    Родитель

---
+++ MyJournal/ViewModels/UserControlsViewModels/Parent/ParentTimetableUserControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyJournal.ViewModels.Base;
+
+namespace MyJournal.ViewModels.UserControlsViewModels.Parent;
+
+public class ParentTimetableUserControlViewModel : ViewModel
+{
+
+}
