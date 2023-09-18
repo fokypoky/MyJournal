f2c3c2e7832adc79500306929ade1d1868a420f2
Anton Petukhov <petuhova805@gmail.com>
2023-09-18 20:45:35 +0300

    Добавил методы получения годов, месяцев и истекающих заданий по объектам студента, предмета, и году

--- MyJournalLibrary/Repositories/EntityRepositories/TasksRepository.cs
+++ MyJournalLibrary/Repositories/EntityRepositories/TasksRepository.cs
@@ -35,12 +35,28 @@ public class TasksRepository : EntityRepository<Task>
             .ToList();
     }
 
+    public ICollection<int> GetTaskYearsByStudentAndSubject(Student student, Subject subject)
+    {
+        return _context.Set<Task>()
+	        .Where(t => t.ClassId == student.ClassId && t.SubjectId == subject.Id)
+	        .Select(t => t.StartDate.Year).Distinct()
+	        .ToList();
+    }
+
     public ICollection<int> GetTaskMonthByClassSubjectAndYear(Class @class, Subject subject, int year)
     {
         return _context.Set<Task>()
             .Where(t => t.ClassId == @class.Id && t.SubjectId == subject.Id && t.StartDate.Year == year)
             .Select(t => t.StartDate.Month).Distinct()
             .ToList();
+    }
+
+    public ICollection<int> GetTaskMonthByStudentSubjectAndYear(Student student, Subject subject, int year)
+    {
+	    return _context.Set<Task>()
+		    .Where(t => t.ClassId == student.ClassId && t.SubjectId == subject.Id && t.StartDate.Year == year)
+		    .Select(t => t.StartDate.Month).Distinct()
+		    .ToList();
     }
 
     public ICollection<Task> GetExpiringTasksByClassSubjectAndDate(Class @class, Subject subject, DateTime currentDate)
@@ -50,4 +66,13 @@ public class TasksRepository : EntityRepository<Task>
                                                && (currentDate.DayOfYear - t.EndDate.DayOfYear <= 7)
             ).ToList();
     }
+
+    public ICollection<Task> GetExpiringTasksByStudentSubjectAndDate(Student student, Subject subject,
+	    DateTime currentDate)
+    {
+	    return _context.Set<Task>()
+		    .Where(t => t.ClassId == student.ClassId && t.SubjectId == subject.Id
+		                                       && (currentDate.DayOfYear - t.EndDate.DayOfYear <= 7)
+		    ).ToList();
+	}
 }
