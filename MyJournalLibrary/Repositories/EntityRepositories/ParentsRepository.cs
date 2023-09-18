51c170a68e771b364082d7f0897857cb9ed918dc
Anton Petukhov <petuhova805@gmail.com>
2023-09-18 17:06:58 +0300

    GetByUserId

--- MyJournalLibrary/Repositories/EntityRepositories/ParentsRepository.cs
+++ MyJournalLibrary/Repositories/EntityRepositories/ParentsRepository.cs
@@ -8,4 +8,9 @@ public class ParentsRepository : EntityRepository<Parent>
     public ParentsRepository(DbContext context) : base(context)
     {
     }
+
+    public Parent? GetByUserId(int userId)
+    {
+	    return _context.Set<Parent>().Where(p => p.ContactsId == userId)?.FirstOrDefault();
+    }
 }
