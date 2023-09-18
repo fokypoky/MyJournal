056775e4d0721627d405c3851434b1a4e2a026cc
Anton Petukhov <petuhova805@gmail.com>
2023-09-18 18:49:16 +0300

    Профиль родителя

--- MyJournal/ViewModels/UserControlsViewModels/Parent/ParentProfileUserControlViewModel.cs
+++ MyJournal/ViewModels/UserControlsViewModels/Parent/ParentProfileUserControlViewModel.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyJournal.Models;
 using MyJournal.ViewModels.Base;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournal.ViewModels.UserControlsViewModels.Parent;
 
 public class ParentProfileUserControlViewModel : ViewModel
 {
+	private MyJournalLibrary.Entities.Parent _parent;
 
+	public MyJournalLibrary.Entities.Parent Parent
+	{
+		get => _parent;
+		set => SetField(ref _parent, value);
+	}
+
+	public ParentProfileUserControlViewModel()
+	{
+		using (var context = new ApplicationContext())
+		{
+			Parent =
+				new ParentsRepository(context)
+					.GetByUserIdWithContactsStudentsAndStudentContacts(ApplicationData.UserId);
+		}
+	}
 }
