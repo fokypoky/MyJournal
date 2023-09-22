02ae8e43c361d97daae6e23ffab2eed0492cc584
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 15:33:04 +0300

    UserControl управления аудиториями

---
+++ MyJournalAdmin/ViewModels/UserControls/Auditories/AuditoriesManagementUserControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyJournalAdmin.ViewModels.Base;
+
+namespace MyJournalAdmin.ViewModels.UserControls.Auditories
+{
+    public class AuditoriesManagementUserControlViewModel : ViewModel
+    {
+    }
+}
