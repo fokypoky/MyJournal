1ff8fe6f4f9dbf0dd9c85ba4b87c56d71dbf12d5
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 14:01:23 +0300

    Авторизация

--- MyJournalAdmin/ViewModels/Windows/Login/LoginWindowViewModel.cs
+++ MyJournalAdmin/ViewModels/Windows/Login/LoginWindowViewModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using MyJournalAdmin.Infrastructure.Commands;
+using MyJournalAdmin.Infrastructure.Repositories;
+using MyJournalAdmin.Models;
 using MyJournalAdmin.ViewModels.Base;
+using MyJournalAdmin.Views.Notifiers.Implementation;
+using MyJournalAdmin.Views.Notifiers.Interfaces;
+using MyJournalAdmin.Views.Windows.Main;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournalAdmin.ViewModels.Windows.Login
 {
@@ -17,6 +23,8 @@ namespace MyJournalAdmin.ViewModels.Windows.Login
 		private bool _needsToSaveLogin;
 
 		#region Public fields
+
+		public INotifier Notifier;
 
 		public string Login
 		{
@@ -47,7 +55,7 @@ namespace MyJournalAdmin.ViewModels.Windows.Login
 
 		public ICommand LoginCommand
 		{
-			get => new RelayCommand(LogIn);
+			get => new RelayCommand(LogIn, CanConectToDb);
 		}
 
 		public ICommand OpenConnectionSettingsCommand
@@ -59,15 +67,63 @@ namespace MyJournalAdmin.ViewModels.Windows.Login
 
 		#region Command functions
 
-		private void RememberLogin(object parameter) { }
-		private void LogIn(object parameter) { }
+		private void RememberLogin(object parameter) => NeedsToSaveLogin = !NeedsToSaveLogin;
+
+		private void LogIn(object parameter)
+		{
+			using (var context = new ApplicationContext())
+			{
+				var contactsRepository = new ContactsRepository(context);
+				var contact = contactsRepository.GetByLogin(Login, Password);
+
+				if (contact == null)
+				{
+					Notifier.Notify("Такого пользователя не существует");
+					return;
+				}
+
+				ApplicationData.UserId = contact.Id;
+
+				if (contact.UserRole.Rolename != "admin")
+				{
+					Notifier.Notify("Недостаточно привилегий");
+					return;
+				}
+
+				var mainWindow = new MainWindow();
+				mainWindow.Show();
+
+				if (parameter is Window)
+				{
+					var currentWindow = (Window) parameter;
+					currentWindow.Close();
+				}
+			}
+		}
+
+		private bool CanConectToDb(object parameter)
+		{
+			using (var context = new ApplicationContext())
+			{
+				try
+				{
+					return context.Database.CanConnect();
+				}
+				catch
+				{
+					return false;
+				}
+			}
+		}
+
 		private void OpenConnectionSettings(object parameter) { }
+
 
 		#endregion
 
 		public LoginWindowViewModel()
 		{
-			
+			Notifier = new MessageBoxNotifier();
 		}
 
 	}
