3182f11425fd26f8d55f8c752ceca8b958707767
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 16:07:47 +0300

    Подключение всех контролов управления сущностями

--- MyJournalAdmin/ViewModels/Windows/Main/MainWindowViewModel.cs
+++ MyJournalAdmin/ViewModels/Windows/Main/MainWindowViewModel.cs
@@ -1,8 +1,45 @@
-﻿using MyJournalAdmin.ViewModels.Base;
+﻿using System.Windows.Controls;
+using MyJournalAdmin.ViewModels.Base;
+using MyJournalAdmin.Views.UserControls.Auditories;
+using MyJournalAdmin.Views.UserControls.Classes;
+using MyJournalAdmin.Views.UserControls.Employees;
+using MyJournalAdmin.Views.UserControls.Students;
+using MyJournalAdmin.Views.UserControls.Subjects;
+using MyJournalAdmin.Views.UserControls.Timetable;
 
 namespace MyJournalAdmin.ViewModels.Windows.Main
 {
     public class MainWindowViewModel : ViewModel
     {
-    }
+	    public UserControl AuditoriesManagementUserControl
+	    {
+		    get => new AuditoriesManagementUserControl();
+	    }
+
+	    public UserControl ClassesManagementUserControl
+	    {
+		    get => new ClassesManagementUserControl();
+	    }
+
+	    public UserControl EmployeesManagementUserControl
+	    {
+		    get => new EmployeesManagementUserControl();
+	    }
+
+	    public UserControl StudentsManagementUserControl
+	    {
+		    get => new StudentsManagementUserControl();
+	    }
+
+	    public UserControl SubjectsManagementUserControl
+	    {
+		    get => new SubjectsManagementUserControl();
+	    }
+
+	    public UserControl TimetableManagementUserControl
+	    {
+		    get => new TimetableManagementUserControl();
+	    }
+
+	}
 }
