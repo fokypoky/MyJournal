960793780dfcc379708c0b4552f91efe8a8e3951
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 14:12:59 +0300

    login data to json saving

---
+++ MyJournalAdmin/Models/LoginData.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyJournalAdmin.Models
+{
+    public class LoginData
+    {
+	    [DataMember] public string Login { get; set; }
+	    [DataMember] public string Password { get; set; }
+
+	    public LoginData(string login, string password)
+	    {
+		    Login = login;
+		    Password = password;
+	    }
+	}
+}
