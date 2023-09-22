f38b16d914cfab7f80a2f399c3102089ab38ff32
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 16:06:58 +0300

    UserControl управления расписанием

---
+++ MyJournalAdmin/ViewModels/UserControls/Timetable/TimetableManagementUserControlViewModel.cs
@@ -0,0 +1,8 @@
+﻿using MyJournalAdmin.ViewModels.Base;
+
+namespace MyJournalAdmin.ViewModels.UserControls.Timetable
+{
+    public class TimetableManagementUserControlViewModel : ViewModel
+    {
+    }
+}
