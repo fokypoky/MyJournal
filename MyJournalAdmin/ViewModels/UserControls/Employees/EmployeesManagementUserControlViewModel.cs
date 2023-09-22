59ea027c80a47bdad210edff005a090a2a391013
Anton Petukhov <petuhova805@gmail.com>
2023-09-22 15:49:45 +0300

    UserControl управления персоналом

---
+++ MyJournalAdmin/ViewModels/UserControls/Employees/EmployeesManagementUserControlViewModel.cs
@@ -0,0 +1,8 @@
+﻿using MyJournalAdmin.ViewModels.Base;
+
+namespace MyJournalAdmin.ViewModels.UserControls.Employees
+{
+    public class EmployeesManagementUserControlViewModel : ViewModel
+    {
+    }
+}
