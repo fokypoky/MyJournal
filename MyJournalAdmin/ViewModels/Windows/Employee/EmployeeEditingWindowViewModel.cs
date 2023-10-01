67c8e521faec98bd10aafad5348922131974dbad
Anton Petukhov <petuhova805@gmail.com>
2023-10-01 21:46:10 +0300

    Обновление контактов сотрудника

--- MyJournalAdmin/ViewModels/Windows/Employee/EmployeeEditingWindowViewModel.cs
+++ MyJournalAdmin/ViewModels/Windows/Employee/EmployeeEditingWindowViewModel.cs
@@ -1,22 +1,41 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using MyJournalAdmin.Infrastructure.Commands;
+using MyJournalAdmin.Infrastructure.Repositories;
 using MyJournalAdmin.Models.Messenging;
 using MyJournalAdmin.Models.Messenging.MessageTypes;
 using MyJournalAdmin.ViewModels.Base;
+using MyJournalAdmin.Views.Notifiers.Implementation;
+using MyJournalAdmin.Views.Notifiers.Interfaces;
+using MyJournalLibrary.Entities;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournalAdmin.ViewModels.Windows.Employee
 {
 	public class EmployeeEditingWindowViewModel : ViewModel
 	{
 		private MyJournalLibrary.Entities.Employee _selectedEmployee;
-		
+
+		private ObservableCollection<Class> _classes;
+		private ObservableCollection<Subject> _subjects;
+
+		private INotifier _notifier;
+
+		private string _newPassword;
+
 		#region Public fields
 
 		public MyJournalLibrary.Entities.Employee SelectedEmployee
 		{
 			get => _selectedEmployee;
 			set => SetField(ref _selectedEmployee, value);
+		}
+
+		public string NewPassword
+		{
+			get => _newPassword;
+			set => SetField(ref _newPassword, value);
 		}
 
 		#endregion
@@ -37,14 +56,51 @@ namespace MyJournalAdmin.ViewModels.Windows.Employee
 
 		#region Command functions
 
-		private void ApplyContactChanges(object parameter) { }
+		private void ApplyContactChanges(object parameter)
+		{
+			if (!IsContactsInputValid())
+			{
+				_notifier.Notify("Ошибка ввода. Поля не должны быть пустыми");
+				return;
+			}
+
+			if (!String.IsNullOrEmpty(NewPassword))
+			{
+				// TODO: хэшировать пароль
+				SelectedEmployee.Contacts.Password = NewPassword;
+			}
+
+			using (var context = new ApplicationContext())
+			{
+				var contactsRepository = new ContactsRepository(context);
+				contactsRepository.Update(SelectedEmployee.Contacts);
+				_notifier.Notify("Данные обновлены");
+			}
+		}
 		
 		private void GeneratePassword(object parameter) { }
 
 		#endregion
 
+		#region Public collections
+
+		public ObservableCollection<Class> Classes
+		{
+			get => _classes;
+			set => SetField(ref _classes, value);
+		}
+
+		public ObservableCollection<Subject> Subjects
+		{
+			get => _subjects;
+			set => SetField(ref _subjects, value);
+		}
+
+		#endregion
+
 		public EmployeeEditingWindowViewModel()
 		{
+			_notifier = new MessageBoxNotifier();
 			WindowMessenger.MessageSender += OnMessageReceived;
 		}
 
@@ -55,8 +111,48 @@ namespace MyJournalAdmin.ViewModels.Windows.Employee
 				var employeeMessage = (EmployeeMessage)e;
 				SelectedEmployee = employeeMessage.Employee;
 				
+				LoadData();
+
 				WindowMessenger.MessageSender -= OnMessageReceived;
 			}
+		}
+
+		private void LoadData()
+		{
+			using (var context = new ApplicationContext())
+			{
+
+			}
+		}
+
+		private bool IsContactsInputValid()
+		{
+			if (String.IsNullOrEmpty(SelectedEmployee.Contacts.Surname))
+			{
+				return false;
+			}
+
+			if (String.IsNullOrEmpty(SelectedEmployee.Contacts.Name))
+			{
+				return false;
+			}
+
+			if (String.IsNullOrEmpty(SelectedEmployee.Contacts.Midname))
+			{
+				return false;
+			}
+
+			if (String.IsNullOrEmpty(SelectedEmployee.Contacts.PhoneNumber))
+			{
+				return false;
+			}
+
+			if (String.IsNullOrEmpty(SelectedEmployee.Contacts.Email))
+			{
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
