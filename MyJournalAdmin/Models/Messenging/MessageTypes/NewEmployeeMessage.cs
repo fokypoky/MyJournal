6962a386548555bb4553c44b70159fad10567e0d
Anton Petukhov <petuhova805@gmail.com>
2023-10-05 18:27:52 +0300

    Сообщение, передаваемое изAddNewEmployeeWindow в EmployeesManagementUserControlViewModel

---
+++ MyJournalAdmin/Models/Messenging/MessageTypes/NewEmployeeMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MyJournalLibrary.Entities;
+
+namespace MyJournalAdmin.Models.Messenging.MessageTypes
+{
+    class NewEmployeeMessage : EventArgs
+    {
+	    public Employee Employee { get; set; }
+    }
+}
