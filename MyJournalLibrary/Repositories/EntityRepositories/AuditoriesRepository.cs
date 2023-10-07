6da264846c8d9957185825a24d2070b2a1c5915c
Anton Petukhov <petuhova805@gmail.com>
2023-10-07 21:47:49 +0300

    Получение всех аудиторий и проверка уникальности номера аудитоии

--- MyJournalLibrary/Repositories/EntityRepositories/AuditoriesRepository.cs
+++ MyJournalLibrary/Repositories/EntityRepositories/AuditoriesRepository.cs
@@ -8,4 +8,18 @@ public class AuditoriesRepository : EntityRepository<Auditory>
     public AuditoriesRepository(DbContext context) : base(context)
     {
     }
+
+    public bool IsAuditoryNumberExists(string auditoryNumber)
+    {
+        return _context.Set<Auditory>()
+	        .FirstOrDefault(a => a.AuditoryNumber == auditoryNumber) is not null;
+    }
+
+    public ICollection<Auditory> GetAll()
+    {
+        return _context.Set<Auditory>()
+	        .Include(a => a.Classes)
+	        .ToList();
+    }
+
 }
