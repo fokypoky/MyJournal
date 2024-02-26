22f44eeb8579b8ed1e46d76d805fd7bb57b3ade6
Anton Petukhov <petuhova805@gmail.com>
2024-02-26 15:09:01 +0300

    fix: Устранение неполадок взаимодействия с бд

--- MyJournalLibrary/Repositories/EntityRepositories/EntityRepository.cs
+++ MyJournalLibrary/Repositories/EntityRepositories/EntityRepository.cs
@@ -15,37 +15,37 @@ public class EntityRepository<T> : IEntityRepository<T> where T : class
 
     public virtual void Add(T entity)
     {
-        _context.Add(entity);
+        _context.Set<T>().Add(entity);
         _context.SaveChanges();
     }
 
     public virtual void AddRange(IEnumerable<T> entities)
     {
-        _context.AddRange(entities);
+        _context.Set<T>().AddRange(entities);
         _context.SaveChanges();
     }
 
     public virtual void Remove(T entity)
     {
-        _context.Remove(entity);
+        _context.Set<T>().Remove(entity);
         _context.SaveChanges();
     }
 
     public virtual void RemoveRange(IEnumerable<T> entities)
     {
-        _context.RemoveRange(entities);
+        _context.Set<T>().RemoveRange(entities);
         _context.SaveChanges();
     }
 
     public virtual void Update(T entity)
     {
-        _context.Update(entity);
+        _context.Set<T>().Update(entity);
         _context.SaveChanges();
     }
 
     public virtual void UpdateRange(IEnumerable<T> entities)
     {
-        _context.UpdateRange(entities);
+        _context.Set<T>().UpdateRange(entities);
         _context.SaveChanges();
     }
 }
