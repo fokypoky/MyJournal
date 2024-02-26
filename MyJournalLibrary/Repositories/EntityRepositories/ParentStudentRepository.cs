e661cfa1ee13e38936e983682009c9c14695c01d
Anton Petukhov <petuhova805@gmail.com>
2024-02-26 15:10:21 +0300

    feat: Репозиторий для сущности "Родитель-Ученик"

---
+++ MyJournalLibrary/Repositories/EntityRepositories/ParentStudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyJournalLibrary.Entities;
+
+namespace MyJournalLibrary.Repositories.EntityRepositories
+{
+	public class ParentStudentRepository : EntityRepository<ParentStudent>
+	{
+		public ParentStudentRepository(DbContext context) : base(context)
+		{
+		}
+
+		public ICollection<ParentStudent> GetByStudent(Student student)
+		{
+			return _context.Set<ParentStudent>()
+				.Where(ps => ps.StudentId == student.Id)
+				.ToList();
+		}
+
+		public void RemoveStudent(List<ParentStudent> parentStudents)
+		{
+			RemoveRange(parentStudents);
+		}
+	}
+}
