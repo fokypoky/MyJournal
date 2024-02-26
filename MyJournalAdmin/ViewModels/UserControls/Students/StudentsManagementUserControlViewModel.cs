eefd58c69767bbb78156430a27d34cc11a69ad5a
Anton Petukhov <petuhova805@gmail.com>
2024-02-26 15:12:24 +0300

    feat: UserControl управления учениками; Удаление ученика

--- MyJournalAdmin/ViewModels/UserControls/Students/StudentsManagementUserControlViewModel.cs
+++ MyJournalAdmin/ViewModels/UserControls/Students/StudentsManagementUserControlViewModel.cs
@@ -1,8 +1,95 @@
-﻿using MyJournalAdmin.ViewModels.Base;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MyJournalAdmin.Infrastructure.Commands;
+using MyJournalAdmin.Infrastructure.Repositories;
+using MyJournalAdmin.ViewModels.Base;
+using MyJournalAdmin.Views.Notifiers.Implementation;
+using MyJournalAdmin.Views.Notifiers.Interfaces;
+using MyJournalLibrary.Entities;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournalAdmin.ViewModels.UserControls.Students
 {
-    public class StudentsManagementUserControlViewModel : ViewModel
-    {
-    }
+	public class StudentsManagementUserControlViewModel : ViewModel
+	{
+		private ObservableCollection<Student>? _students;
+		private Student? _selectedStudent;
+		private readonly INotifier _notifier;
+
+		public ObservableCollection<Student>? Students
+		{
+			get => _students;
+			set => SetField(ref _students, value);
+		}
+
+		public Student? SelectedStudent
+		{
+			get => _selectedStudent;
+			set => SetField(ref _selectedStudent, value);
+		}
+
+		#region Commands
+
+		public ICommand AddNewStudentCommand
+		{
+			get => new RelayCommand(AddNewStudent);
+		}
+
+		public ICommand UpdateStudentCommand
+		{
+			get => new RelayCommand(UpdateStudent);
+		}
+
+		public ICommand DeleteStudentCommand
+		{
+			get => new RelayCommand(DeleteStudent);
+		}
+
+		#endregion
+
+		#region Command methods
+
+		private void AddNewStudent(object parameter) { }
+
+		private void UpdateStudent(object parameter) { }
+
+		private void DeleteStudent(object parameter)
+		{
+			if (SelectedStudent is null)
+			{
+				_notifier.Notify("Ученик не выбран");
+				return;
+			}
+			
+			using (var context = new ApplicationContext())
+			{
+				var marksRepository = new MarksRepository(context);
+				var parentStudentRepository = new ParentStudentRepository(context);
+
+				marksRepository.RemoveRange(marksRepository.GetByStudent(SelectedStudent));
+				parentStudentRepository.RemoveRange(parentStudentRepository.GetByStudent(SelectedStudent));
+				new StudentsRepository(context).Remove(SelectedStudent);
+				new ContactsRepository(context).Remove(SelectedStudent.Contacts);
+
+				Students?.Remove(SelectedStudent);
+			}
+
+			_notifier.Notify("Ученик удален");
+			SelectedStudent = null;
+		}
+
+		#endregion
+
+		public StudentsManagementUserControlViewModel()
+		{
+			using (var context = new ApplicationContext())
+			{
+				Students = new ObservableCollection<Student>(
+					new StudentsRepository(context).GetAllWithClassAndContacts()
+				);
+			}
+
+			_notifier = new MessageBoxNotifier();
+		}
+	}
 }
