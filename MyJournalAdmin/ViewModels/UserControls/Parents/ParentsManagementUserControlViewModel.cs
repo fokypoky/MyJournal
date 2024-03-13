0c71a5a8f79f5c77b9033e9878026eaa74f03087
Anton <petuhova805@gmail.com>
2024-03-13 15:59:19 +0300

    feat: Remove parent

--- MyJournalAdmin/ViewModels/UserControls/Parents/ParentsManagementUserControlViewModel.cs
+++ MyJournalAdmin/ViewModels/UserControls/Parents/ParentsManagementUserControlViewModel.cs
@@ -1,9 +1,103 @@
-﻿using MyJournalAdmin.ViewModels.Base;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using MyJournalAdmin.Infrastructure.Commands;
+using MyJournalAdmin.Infrastructure.Repositories;
+using MyJournalAdmin.ViewModels.Base;
+using MyJournalAdmin.Views.Notifiers.Implementation;
+using MyJournalAdmin.Views.Notifiers.Interfaces;
+using MyJournalLibrary.Entities;
+using MyJournalLibrary.Repositories.EntityRepositories;
 
 namespace MyJournalAdmin.ViewModels.UserControls.Parents
 {
     public class ParentsManagementUserControlViewModel : ViewModel
     {
+        private INotifier _notifier;
+
+        private ObservableCollection<Parent> _parents;
+        private Parent? _selectedParent;
+
+        #region Public fields
+
+        public Parent? SelectedParent
+        {
+            get => _selectedParent;
+            set => SetField(ref _selectedParent, value);
+        }
+
+        #endregion
         
+        #region Collections
+
+        public ObservableCollection<Parent> Parents
+        {
+            get => _parents;
+            set => SetField(ref _parents, value);
+        }
+
+        #endregion"
+
+        #region Commands
+
+        public ICommand AddParentCommand
+        {
+            get => new RelayCommand(AddParent);
+        }
+
+        public ICommand RemoveParentCommand
+        {
+            get => new RelayCommand(RemoveParent);
+        }
+
+        public ICommand UpdateParentCommand
+        {
+            get => new RelayCommand(UpdateParent);
+        }
+
+        #endregion
+
+        #region Command methods
+
+        private void AddParent(object parameter) { }
+
+        private void RemoveParent(object parameter)
+        {
+            if (SelectedParent is null)
+            {
+                _notifier.Notify("Выберите родителя");
+                return;
+            }
+
+            using (var context = new ApplicationContext())
+            {
+                var parentStudentRepository = new ParentStudentRepository(context);
+                var parentStudents = parentStudentRepository.GetByParent(SelectedParent);
+                parentStudentRepository.RemoveRange(parentStudents);
+                
+                new ParentsRepository(context).Remove(SelectedParent);
+                new ContactsRepository(context).Remove(SelectedParent.Contacts);
+                
+                _notifier.Notify("Выбранный родитель удален");
+                Parents.Remove(SelectedParent);
+
+                SelectedParent = null;
+            }
+        }
+        private void UpdateParent(object parameter) { }
+
+        #endregion
+        
+        public ParentsManagementUserControlViewModel()
+        {
+            _notifier = new MessageBoxNotifier();
+            using (var context = new ApplicationContext())
+            {
+                Parents = new ObservableCollection<Parent>(
+                    new ParentsRepository(context).GetAllWithContacts()
+                        .OrderBy(p => p.Contacts.Surname)
+                );
+            }
+        }
     }
 }
