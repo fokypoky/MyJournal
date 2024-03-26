d0fd788c632bb47a695f1da64d790bda7b8af494
Anton <petuhova805@gmail.com>
2024-03-26 14:02:04 +0300

    feat: Репозиторий сущности сотрудник-предмет

---
+++ MyJournalLibrary/Repositories/EntityRepositories/EmployeeSubjectRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyJournalLibrary.Entities;
+
+namespace MyJournalLibrary.Repositories.EntityRepositories
+{
+	public class EmployeeSubjectRepository : EntityRepository<EmployeeSubject>
+	{
+		public EmployeeSubjectRepository(DbContext context) : base(context)
+		{
+		}
+
+		public ICollection<EmployeeSubject> GetEmployeesWithContactsBySubject(Subject subject)
+		{
+			return _context.Set<EmployeeSubject>()
+				.Where(es => es.SubjectId == subject.Id)
+				.Include(es => es.Employee)
+				.ThenInclude(e => e.Contacts)
+				.ToList();
+		}
+	}
+}
